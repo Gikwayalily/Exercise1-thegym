a00c784fdea738085dd7359560be55475d6750ef
Gikwayalily <gikwayaliliane@gmail.com>
2023-05-14 17:02:31 +0200

    first push on this project to create the Navbar

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Navbar from './router/navbar/navbar'
+
+function App() {
+
+
+  return (
+    <Navbar/>
+  )
+}
+
+export default App
---
+++ src/router/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+
+ import "./navbar.css"
+const Navbar = () => {
+  return (
+    <div>
+        <div className='list1'>
+            CINNAMON
+            <ul>
+                <li>HOME</li>
+                <li>SERVICE</li>
+                <li>ABOUT US</li>
+                <li>CAREER</li>
+                <li>Blog</li>
+            </ul>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
